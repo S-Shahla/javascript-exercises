257fd26730db1e2be3a82d9974a82d34cf2f6acd
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2024-12-28 16:51:37 -0800

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let celsiusTemp = ((fahrenheitTemp - 32) * 5 / 9);
+  if (Math.round(celsiusTemp) === celsiusTemp) return celsiusTemp;
+  return Math.round(celsiusTemp * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let fahrenheitTemp = ((celsiusTemp * 9 / 5) + 32);
+  if (Math.round(fahrenheitTemp) === fahrenheitTemp) return fahrenheitTemp;
+  return Math.round(fahrenheitTemp * 10) / 10; 
 };
 
 // Do not edit below this line
