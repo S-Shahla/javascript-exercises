bc700a80880781e4300a8c194cf78e7d4102b775
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2024-12-29 14:38:14 -0800

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date();
+    let ages = people.map((person) => (person.yearOfDeath || date.getFullYear()) - person.yearOfBirth);
+    const maxValue = Math.max(...ages);
+    return people[ages.indexOf(maxValue)];
 };
 
 // Do not edit below this line
