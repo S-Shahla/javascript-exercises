994f25fcb420f93ea887f785fb883f7615ac55c3
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2024-12-28 16:36:14 -0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let number = 0;
+    startNum = Math.min(num1, num2);
+    endNum = Math.max(num1, num2);
+    if (startNum < 0 || !Number.isInteger(num1) || !Number.isInteger (num2)) return "ERROR";
+    for (let i = startNum; i <= endNum; i++) {
+        number += i;
+    }
+    return number
 };
 
 // Do not edit below this line
