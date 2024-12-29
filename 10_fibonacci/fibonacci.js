500e4130d99a68c316f3b874d3a65f039010c9e7
S-Shahla <143126171+S-Shahla@users.noreply.github.com>
2024-12-29 13:53:59 -0800

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    let result = (num > 0) ? 1: ((num < 0) ? "OOPS" : 0);
+    let last = 0;
+    for (; num > 1; num--) {
+        const transfer = result;
+        result += last;
+        last = transfer;
+    }
+    return result;
 };
 
 // Do not edit below this line
